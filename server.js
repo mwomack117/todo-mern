27b432fa800b6f75597dcfd592df767240511a8c
mwomack <mikewomack2000@yahoo.com>
2019-02-08 14:46:32 -0800

    testing heroku deploy

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes/api");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Connect to the Mongo DB
+mongoose
+  .connect(process.env.MONGODB_URI || process.env.DEV_DB, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log(`Database connected successfully`))
+  .catch(err => console.log(err));
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use("/api", routes);
+
+// app.use((req, res, next) => {
+//   res.send("Welcome to Express");
+// });
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+app.listen(port, () => {
+  console.log(`🌎 ==> Server running on port ${port}`);
+});
